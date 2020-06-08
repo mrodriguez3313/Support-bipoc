89b5704ae5040ee921dc9b7ac8fcb38e230daed6
Marco Rodriguez <marco.rodriguez113@gmail.com>
2020-06-07 19:18:20 -0700

    Shortened base_state.html by making navbar more general. And made tuples in views.py instead of single values to make it happen.

--- website/core/views.py
+++ website/core/views.py
@@ -28,7 +28,7 @@ def zero():
 
 @core.route('/Texas')
 def Texas():
-    return render_template('Texas.html', title="Texas", Content=dict["texas_Content"], states=states_array)
+    return render_template('Texas.html', title="Texas", Content=dict["texas_Content"], states=states_array, donation_page=("Donation Pages TX", "Texas_donate.html"), state_orgs=("Organizations TX", "Texas_org.html"), state_black_businesses=("Black Businesses TX", "Texasbiz.html") )
 
 @core.route('/Texas/donate')
 def Texas_donate():
@@ -44,7 +44,7 @@ def Tex_biz():
 
 @core.route('/California')
 def Cali():
-    return render_template('Cali.html', title="California", Content=dict["cali_Content"])
+    return render_template('Cali.html', title="California", Content=dict["cali_Content"], states=states_array, donation_page=("Donation Pages CA", "Cali_donate.html"), state_orgs=("Organizations CA", "Cali_org.html"), state_black_businesses=("Black Businesses CA","Calibiz.html") )
 
 @core.route('/California/donate')
 def Cali_donate():
@@ -60,7 +60,7 @@ def Cali_biz():
 
 @core.route('/Minnesota')
 def MN():
-    return render_template('Minnesota.html', title="Minnesota", Content=dict["minn_Content"])
+    return render_template('Minnesota.html', title="Minnesota", Content=dict["minn_Content"], states=states_array, donation_page=("Donation Pages MN", "MN_donate.html"), state_orgs=("Organizations MN", "MN_org.html"), state_black_businesses=("Black Businesses MN", "MNbiz.html") )
 
 @core.route('/Minnesota/donate')
 def MN_donate():
