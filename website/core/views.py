790646e07de794bf2635bc55dbeb2de7ca12cc60
Alan Castilleja <alan.castilleja.94@gmail.com>
2020-06-11 00:12:47 -0500

    basic template for all state specific pages and fixed index page for mobile

--- website/core/views.py
+++ website/core/views.py
@@ -1,7 +1,7 @@
 from flask import render_template, Blueprint
 core = Blueprint('core', __name__)
-states_array=["California", "Texas", "Minnesota"]
-
+states_a = ["California", "Texas", "Minnesota"]
+states_array = sorted(states_a)
 dict = {
     "cali_Content" :  "This is where Cali links and content will go.",
     "texas_Content" : "This is where Texas links and content will go.",
@@ -30,19 +30,19 @@ def zero():
 
 @core.route('/Texas')
 def Texas():
-    return render_template('Texas.html', title="Texas", Content=dict["texas_Content"], states=states_array, donation_page=("Donation Pages TX", "core.Texas_donate"), state_orgs=("Organizations TX", "core.Texas_org"), state_black_businesses=("Black Businesses TX", "core.Tex_biz") )
+    return render_template('Texas.html', title="Texas", Content=dict["texas_Content"], states=states_array, donation_page=("Donation Pages TX", "core.Texas_donate"), state_orgs=("Organizations TX", "core.Texas_org"), state_black_businesses=("Black Businesses TX", "core.Tex_biz"))
 
 @core.route('/Texas/donate')
 def Texas_donate():
-    return render_template('Texas_donate.html')
+    return render_template('Texas_donate.html', title="Texas", Content=dict["texas_Content"], states=states_array, donation_page=("Donation Pages TX", "core.Texas_donate"), state_orgs=("Organizations TX", "core.Texas_org"), state_black_businesses=("Black Business TX", "core.Tex_biz"))
 
 @core.route('/Texas/organizations')
 def Texas_org():
-    return render_template('Texas_org.html')
+    return render_template('Texas_org.html', title="Texas", Content=dict["texas_Content"], states=states_array, donation_page=("Donation Pages TX", "core.Texas_donate"), state_orgs=("Organizations TX", "core.Texas_org"), state_black_businesses=("Black Business TX", "core.Tex_biz"))
 
 @core.route('/Texas/BlackBusiness')
 def Tex_biz():
-    return render_template('Texbiz.html')
+    return render_template('Texbiz.html', title="Texas", Content=dict["texas_Content"], states=states_array, donation_page=("Donation Pages TX", "core.Texas_donate"), state_orgs=("Organizations TX", "core.Texas_org"), state_black_businesses=("Black Business TX", "core.Tex_biz"))
 
 @core.route('/California')
 def Cali():
@@ -50,15 +50,15 @@ def Cali():
 
 @core.route('/California/donate')
 def Cali_donate():
-    return render_template('Cali_donate.html')
+    return render_template('Cali_donate.html', title="California", Content=dict["cali_Content"], states=states_array, donation_page=("Donation Pages CA", "core.Cali_donate"), state_orgs=("Organizations CA", "core.Cali_org"), state_black_businesses=("Black Businesses CA","core.Cali_biz") )
 
 @core.route('/California/organizations')
 def Cali_org():
-    return render_template('Cali_org.html')
+    return render_template('Cali_org.html', title="California", Content=dict["cali_Content"], states=states_array, donation_page=("Donation Pages CA", "core.Cali_donate"), state_orgs=("Organizations CA", "core.Cali_org"), state_black_businesses=("Black Businesses CA","core.Cali_biz") )
 
 @core.route('/California/BlackBusiness')
 def Cali_biz():
-    return render_template('Calibiz.html')
+    return render_template('Calibiz.html', title="California", Content=dict["cali_Content"], states=states_array, donation_page=("Donation Pages CA", "core.Cali_donate"), state_orgs=("Organizations CA", "core.Cali_org"), state_black_businesses=("Black Businesses CA","core.Cali_biz") )
 
 @core.route('/Minnesota')
 def MN():
@@ -66,12 +66,12 @@ def MN():
 
 @core.route('/Minnesota/donate')
 def MN_donate():
-    return render_template('MN_donate.html')
+    return render_template('MN_donate.html', title="Minnesota", Content=dict["minn_Content"], states=states_array, donation_page=("Donation Pages MN", "core.MN_donate"), state_orgs=("Organizations MN", "core.MN_org"), state_black_businesses=("Black Businesses MN", "core.MN_biz") )
 
 @core.route('/Minnesota/organizations')
 def MN_org():
-    return render_template('MN_org.html')
+    return render_template('MN_org.html', title="Minnesota", Content=dict["minn_Content"], states=states_array, donation_page=("Donation Pages MN", "core.MN_donate"), state_orgs=("Organizations MN", "core.MN_org"), state_black_businesses=("Black Businesses MN", "core.MN_biz") )
 
 @core.route('/Minnesota/BlackBusiness')
 def MN_biz():
-    return render_template('MNbiz.html')
+    return render_template('MNbiz.html', title="Minnesota", Content=dict["minn_Content"], states=states_array, donation_page=("Donation Pages MN", "core.MN_donate"), state_orgs=("Organizations MN", "core.MN_org"), state_black_businesses=("Black Businesses MN", "core.MN_biz") )
