92b7544929be5ae7527bf61ea7096dc96651c608
Marco Rodriguez <marco.rodriguez113@gmail.com>
2020-07-08 14:04:37 -0700

    shorter version

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
+from flask_frozen import Freezer
+from flask import Flask
 from website import app
 
+freezer = Freezer(app)
+app.config.from_pyfile('settings.py')
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    freezer.freeze()
+    # app.run(debug=True)
